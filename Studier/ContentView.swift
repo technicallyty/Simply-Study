8275bb83f0b9d6f5c20fe4ea8258370065b6fca4
tytech3 <48813565+tytech3@users.noreply.github.com>
2020-03-09 18:16:02 -0700

    Initial Commit

---
+++ Studier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Studier
+//
+//  Created by Tyler Goodman on 3/9/20.
+//  Copyright © 2020 Tyler Goodman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
